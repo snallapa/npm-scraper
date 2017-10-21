5089567bedc87fb32bb3a37dce7f6e2d7f7649c4
Sahith Nallapareddy <sahith.reddy@gmail.com>
2017-10-21 12:17:48 -0400

    connecting to github api

--- convert.py
+++ convert.py
@@ -5,7 +5,10 @@ with open('edges.txt', 'r') as infile:
 
 newlist = [(item.strip()[1:-1].split(",")[0], item.strip()[1:-1].split(",")[1]) for item in list]
 print(newlist[0][0])
+print(newlist[0][1])
+'''
 with open('edges.csv','w') as out:
     csv_out=csv.writer(out)
     for item in newlist:
         csv_out.writerow([item[0].strip()[1:-1], item[1].strip()[1:]])
+'''
--- githubData.py
+++ githubData.py
@@ -59,10 +59,12 @@ weirdPackages = []
 with open('nodes.txt') as nodesFile:    
     nodes = json.load(nodesFile)
 packages = list(nodes.keys())
+packagesWithNoRepos = []
 for package in packages:
     repo = nodes[package].get("repo", None)
     if repo == None or repo == "":
         counter1 = counter1 + 1
+        packagesWithNoRepos.append(package)
         continue
     url = repo
     if type(repo) is dict:
@@ -72,13 +74,44 @@ for package in packages:
     ownerAndRepo = getOwnerAndRepo(url)
     if url == '':
         counter1 = counter1 + 1
+        packagesWithNoRepos.append(package)
         continue
     if ownerAndRepo == None:
         counter2 = counter2 + 1
         weirdPackages.append(url)
         continue
-    
+'''
+with open('edges.txt') as edgesFile:    
+    list = edgesFile.read().split("),")
+edges = [(item.strip()[1:-1].split(",")[0].strip()[1:-1], item.strip()[1:-1].split(",")[1].strip()[1:]) for item in list]
+inDictionary = {}
+outDictionary = {}
+for edge in edges:
+    inDictionary[edge[0]] = inDictionary.get(edge[0], []) + [edge[1]]
+    outDictionary[edge[1]] = inDictionary.get(edge[1], []) + [edge[0]]
+f = open('in-edges.txt', 'w')
+f.write(json.dumps(inDictionary))
+f.close()
+f = open('out-edges.txt', 'w')
+f.write(json.dumps(outDictionary))
+f.close()
+'''
 
+with open('in-edges.txt') as inEdgesFile:    
+    inEdges = json.load(inEdgesFile)
+with open('out-edges.txt') as outEdgesFile:
+    outEdges = json.load(outEdgesFile)
+
+
+degreeDistributionMap = {}
+for package in packagesWithNoRepos:
+    degreeDistributionMap[package] = {"in": len(inEdges.get(package,[])), "out": len(outEdges.get(package, []))}
+numberDistributionMap = {}
+for ins in degreeDistributionMap.values():
+    numberDistributionMap[str(ins["out"])] = numberDistributionMap.get(str(ins["out"]), 0) + 1
+for key, value in numberDistributionMap.items():
+    print("{},{}".format(key, value))
+'''
 print("{} number of packages did not have a repo".format(counter1))
 print("{} number of packages had a different repo".format(counter2))
 bitbucketCount = reduce((lambda count, i: count + ("bitbucket" in i)), weirdPackages, 0)
@@ -87,3 +120,4 @@ github = reduce((lambda count, i: count + ("github" in i)), weirdPackages, 0)
 print("{} number of packages on bitbucket".format(bitbucketCount))
 print("{} number of packages on gitlab".format(gitlab))
 print("{} number of packages on github but malformed".format(github))
+'''
